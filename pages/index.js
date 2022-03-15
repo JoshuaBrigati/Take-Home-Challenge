9fce35ec66e4fa88dbfa15d71a575c3acbdd2e4b
Joshua <joshua.brigati@gmail.com>
2022-03-14 22:58:43 -0500

    some comment clean up

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,21 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    // Used to get actually hieght for mobile browsers.
+    // Mobile bowsers have that bottom nav bar that messes up '100vh'
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    // We listen to the resize event
     window.addEventListener('resize', () => {
-      // We execute the same script as before
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
+
+    return () => {
+      window.removeEventListener('resize');
+    }
   })
+
   return (
     <div className='container'>
       <Head>
